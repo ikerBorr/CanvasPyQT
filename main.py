3be50d8d96cd0ef7429148688a400077ca24a32f
ikerBorr <ikerborr@outlook.es>
2023-03-13 18:38:44 +0100

    improve

--- main.py
+++ main.py
@@ -19,38 +19,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.SPOffset.setValue(0.5)
         self.SPGap.setValue(0.5)
 
-        self.events()
+        self.__events()
 
-    def events(self) -> None:
-        self.SPCanvasX.valueChanged.connect(self.resize_canvas)
-        self.SPCanvasY.valueChanged.connect(self.resize_canvas)
-        self.SPCopyX.valueChanged.connect(self.reload)
-        self.SPCopyY.valueChanged.connect(self.reload)
-        self.SPGap.valueChanged.connect(self.reload)
-        self.SPOffset.valueChanged.connect(self.reload)
-        self.SPNCopies.valueChanged.connect(self.calculate_copies)
+    def __events(self) -> None:
+        self.SPCanvasX.valueChanged.connect(self.__resize_canvas)
+        self.SPCanvasY.valueChanged.connect(self.__resize_canvas)
+        self.SPCopyX.valueChanged.connect(self.__reload)
+        self.SPCopyY.valueChanged.connect(self.__reload)
+        self.SPGap.valueChanged.connect(self.__reload)
+        self.SPOffset.valueChanged.connect(self.__reload)
+        self.SPNCopies.valueChanged.connect(self.__calculate_copies)
 
-    def reload(self) -> None:
+    def __reload(self) -> None:
         self.__copies = self.__canvas.generate_print(self.SPCopyX.value(), self.SPCopyY.value(),
                                                      self.SPOffset.value(), self.SPGap.value())
-        self.calculate_copies()
+        self.__calculate_copies()
 
-    def calculate_copies(self):
+    def __calculate_copies(self):
         if self.__copies > 0:
             num_sheet = math.ceil(self.SPNCopies.value() / self.__copies)
             self.LBSheet.setText(str(num_sheet))
             self.LBCopies.setText(str(num_sheet * self.__copies))
             self.LBCopies.setStyleSheet("color:")
             self.LBSheet.setStyleSheet("color:")
+
+            area_util = (self.SPCopyX.value() * self.SPCopyY.value() * self.__copies) / \
+                        (self.SPCanvasX.value() * self.SPCanvasY.value())
+            self.LBUtil.setText(str("{:.1f}".format(area_util * 100)) + "%")
         else:
             self.LBSheet.setText("err")
             self.LBCopies.setText("err")
             self.LBCopies.setStyleSheet("color: red")
             self.LBSheet.setStyleSheet("color: red")
 
-    def resize_canvas(self) -> None:
+    def __resize_canvas(self) -> None:
         self.__canvas.resize(self.SPCanvasX.value(), self.SPCanvasY.value())
-        self.reload()
+        self.__reload()
 
 
 # ---- MAIN -----
