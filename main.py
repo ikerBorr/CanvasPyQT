89f3178fc0924b507b8b3eabed92bcb3417fb593
ikerBorr <ikerborr@outlook.es>
2023-03-13 18:12:28 +0100

    improve

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ import math
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.__copies = 0
         self.setupUi(self)
         self.show()
 
+        self.__canvas = CCanvas(self.GVCanvas)
+        self.__copies = 0
         self.SPOffset.setValue(0.5)
         self.SPGap.setValue(0.5)
 
-        self.__canvas = CCanvas(self.GVCanvas)
+        self.events()
+
+    def events(self) -> None:
         self.SPCanvasX.valueChanged.connect(self.resize_canvas)
         self.SPCanvasY.valueChanged.connect(self.resize_canvas)
         self.SPCopyX.valueChanged.connect(self.reload)
@@ -26,14 +29,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.SPGap.valueChanged.connect(self.reload)
         self.SPOffset.valueChanged.connect(self.reload)
         self.SPNCopies.valueChanged.connect(self.calculate_copies)
-    
+
     def reload(self) -> None:
         self.__copies = self.__canvas.generate_print(self.SPCopyX.value(), self.SPCopyY.value(),
                                                      self.SPOffset.value(), self.SPGap.value())
         self.calculate_copies()
 
     def calculate_copies(self):
-
         if self.__copies > 0:
             num_sheet = math.ceil(self.SPNCopies.value() / self.__copies)
             self.LBSheet.setText(str(num_sheet))
@@ -51,6 +53,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.reload()
 
 
-app = QApplication(sys.argv)
-w = MainWindow()
-app.exec()
+# ---- MAIN -----
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    app.exec()
